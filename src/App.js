7418508fea109f10f2c0c1eaf479a96c5c53f4e1
01Clarian <clarionnorth@gmail.com>
2019-06-08 16:44:33 -0400

    app contact

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import ArtistNews from './components/ArtistNews'
 import ArtistNewsPage from './components/ArtistNewsPage'
+import Contact from './components/contact/Contact'
+
 function App() {
   return (
+    <div>
     <Router>
     <Fragment>
       <Route exact path="/reactheadlesstest/" component={ArtistNews} />
       <Route exact path="/reactheadlesstest/artistnews/:id" component={ArtistNewsPage} />
     </Fragment>
     </Router>
+     <Contact />
+     </div>
   );
 }
 
